5266221a633b5b49bb4bd23f24a30ce89fb3921e
moise10r <moiserushanika2006@gmail.com>
2021-10-22 20:45:25 +0200

    Get customer by cardId.

--- routers/customer.js
+++ routers/customer.js
@@ -48,6 +48,14 @@ router.get("/api/customer", async (req, res) => {
 	res.status(200).send(customer);
 });
 
+
+router.get("/api/customer", async (req, res) => {
+	const customer = await Customer.findOne({ cardId: req.query.cardId });
+	if (!customer)
+		return res.status(400).send("There is no a customer with that plateNumber");
+	res.status(200).send(customer);
+});
+
 router.get("/api/customer/", async (req, res) => {
 	const customer = await Customer.findOne({ plateNumber: req.query.plateNumber });
 	if (!customer)
