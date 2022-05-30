c8029fd4e36be6ab2dd2192298b79be9ccc13d50
moise10r <moiserushanika2006@gmail.com>
2022-05-31 00:08:56 +0200

    Test request

--- routers/customer.js
+++ routers/customer.js
@@ -105,14 +105,15 @@ router.get("/api/customers", async (req, res) => {
 
 router.post("/api/sendAlert", async (req, res) => {
 	const { cNumber } = req.body;
-	client.messages
-		.create({
-			to: cNumber,
-			from: '+15156057424',
-			body: 'Hello from Parking, It seems like your car is blocking the parking, you have 3 seconds to move it to not be slapped',
-		})
-		.then(message => res.send(`Alert with id ${message.sid} was sent to ${message.to} successfully`))
-		.catch((err) => res.send('err' + typeof cNumber));
+	// client.messages
+	// 	.create({
+	// 		to: cNumber,
+	// 		from: '+15156057424',
+	// 		body: 'Hello from Parking, It seems like your car is blocking the parking, you have 3 seconds to move it to not be slapped',
+	// 	})
+	// 	.then(message => res.send(`Alert with id ${message.sid} was sent to ${message.to} successfully`))
+	// 	.catch((err) => res.send('err' + typeof cNumber));
+		return res.send(typeof cNumber);
 });
 
 module.exports = router;
