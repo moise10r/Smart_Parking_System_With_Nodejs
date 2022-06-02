a4b149745a94f87d06f84761be5fd1f3e175dac7
moise10r <moiserushanika2006@gmail.com>
2022-06-02 22:51:19 +0200

    Change twilo config.

--- routers/customer.js
+++ routers/customer.js
@@ -107,16 +107,22 @@ router.post("/api/sendAlert", async (req, res) => {
 	const accountSid = process.env.ACCOUNT_SID;
 	const authToken = process.env.AUTH_TOKEN;
 	const client = require('twilio')(accountSid, authToken);
-
+	let mess;
 	client.messages
 		.create({
 			to: cNumber,
 			from: '+15156057424',
 			body: 'Hello from Parking, It seems like your car is blocking the parking, you have 3 seconds to move it to not be slapped',
 		})
-		.then(message => console.log(`Alert with id ${message.sid} was sent to ${message.to} successfully`))
-		.catch((err) => console.log('err' + err));
-	return res.send(cNumber)
+		.then(message => {
+			mess = message
+			console.log(`Alert with id ${message.sid} was sent to ${message.to} successfully`)
+		})
+		.catch((err) =>{ 
+			mess = err
+			console.log('err' + err)
+		});
+	return res.send(mess)
 });
 
 module.exports = router;
